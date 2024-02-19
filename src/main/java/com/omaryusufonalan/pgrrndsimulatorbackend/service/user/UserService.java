b9cf33658b69a6e0bda978b814ccf1bb0d1afcf3
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 00:10:24 +0300

    Implement UserCRUD and GetUserDTO

--- src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/service/user/UserService.java
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/service/user/UserService.java
@@ -3,29 +3,43 @@ package com.omaryusufonalan.pgrrndsimulatorbackend.service.user;
 import com.omaryusufonalan.pgrrndsimulatorbackend.dto.user.UserRequest;
 import com.omaryusufonalan.pgrrndsimulatorbackend.dto.user.UserResponse;
 import com.omaryusufonalan.pgrrndsimulatorbackend.entity.User;
+import com.omaryusufonalan.pgrrndsimulatorbackend.mapper.UserMapper;
+import com.omaryusufonalan.pgrrndsimulatorbackend.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserCRUD, GetUserDTO {
-    @Override
-    public UserResponse getResponseById(Long id) {
-        return null;
-    }
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User getById(Long id) {
-        return null;
+        return userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User with ID " + id + " not found"));
     }
 
     @Override
     public UserResponse update(Long id, UserRequest userRequest) {
-        return null;
+        User userToBeUpdated = getById(id);
+        userRequest.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+
+        userMapper.update(userToBeUpdated, userRequest);
+
+        return userMapper.asUserResponse(userRepository.save(userToBeUpdated));
     }
 
     @Override
     public void delete(Long id) {
+        userRepository.delete(getById(id));
+    }
 
+    @Override
+    public UserResponse getResponseById(Long id) {
+        return userMapper.asUserResponse(getById(id));
     }
 }
