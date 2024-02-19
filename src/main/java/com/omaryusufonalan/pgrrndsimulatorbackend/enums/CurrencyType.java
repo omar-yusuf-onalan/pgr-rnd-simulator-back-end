ce977a968a61123bb286128ece4d428d9e23ce5f
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 00:25:23 +0300

    Create CurrencyType.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/enums/CurrencyType.java
@@ -0,0 +1,11 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.enums;
+
+public enum CurrencyType {
+    RAINBOW_CARD,
+    BLACK_CARD,
+    EVENT_TICKET,
+    BASIC_CONSTRUCT_TICKET,
+    TARGET_WEAPON_TICKET,
+    BASIC_WEAPON_TICKET,
+    CUB_TICKET
+}
