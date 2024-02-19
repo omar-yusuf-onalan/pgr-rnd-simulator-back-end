70e901b3fb38b3f4dddcd703650e60a9ed2325eb
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 00:22:10 +0300

    Create Player.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/entity/user/Player.java
@@ -0,0 +1,9 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.entity.user;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("PLAYER")
+public class Player {
+}
