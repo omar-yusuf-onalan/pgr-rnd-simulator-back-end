215e8dc5ac5ec1805058e8637b8a875f3da08e5c
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-19 23:34:50 +0300

    Create workspace

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/PgrRndSimulatorBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgrRndSimulatorBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgrRndSimulatorBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omaryusufonalan/pgrrndsimulatorbackend/PgrRndSimulatorBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgrRndSimulatorBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
