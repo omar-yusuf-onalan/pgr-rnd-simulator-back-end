07686af9284d80a99b631fbbaa150cc5c6fcc4b6
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 00:12:12 +0300

    Create UserController.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.controller;
+
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.user.UserRequest;
+import com.omaryusufonalan.pgrrndsimulatorbackend.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.omaryusufonalan.pgrrndsimulatorbackend.config.ApplicationConfig.BASE_URL;
+
+@RestController
+@RequestMapping(BASE_URL + "user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getResponseById(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getResponseById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody UserRequest userRequest) {
+        return new ResponseEntity<>(userService.update(id, userRequest), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
