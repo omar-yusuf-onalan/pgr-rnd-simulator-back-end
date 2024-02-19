318396df8f6333286900f0ec8c1904cbba112553
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 00:25:36 +0300

    Create Currency.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/entity/Currency.java
@@ -0,0 +1,28 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.entity;
+
+import com.omaryusufonalan.pgrrndsimulatorbackend.entity.user.Player;
+import com.omaryusufonalan.pgrrndsimulatorbackend.enums.CurrencyType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Currency {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private int amount;
+
+    @Enumerated(EnumType.STRING)
+    private CurrencyType type;
+
+    @ManyToOne
+    private Player player;
+}
--- src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/entity/user/Player.java
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/entity/user/Player.java
@@ -1,9 +1,13 @@
 package com.omaryusufonalan.pgrrndsimulatorbackend.entity.user;
 
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
+import com.omaryusufonalan.pgrrndsimulatorbackend.entity.Currency;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 @DiscriminatorValue("PLAYER")
 public class Player {
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "player", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Set<Currency> currencies;
 }
