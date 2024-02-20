f361757c56f012c15cfe47bff013660bc1febf53
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 15:41:44 +0300

    Add memory methods

--- src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/controller/SupplyController.java
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/controller/SupplyController.java
@@ -1,6 +1,7 @@
 package com.omaryusufonalan.pgrrndsimulatorbackend.controller;
 
 import com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.construct.ConstructRequest;
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.memory.MemoryRequest;
 import com.omaryusufonalan.pgrrndsimulatorbackend.service.supply.SupplyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,8 +16,8 @@ import static com.omaryusufonalan.pgrrndsimulatorbackend.config.ApplicationConfi
 public class SupplyController {
     private final SupplyService supplyService;
     
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getResponseById(@PathVariable Long id) {
+    @GetMapping("/construct/{id}")
+    public ResponseEntity<?> getConstructResponseById(@PathVariable Long id) {
         return new ResponseEntity<>(supplyService.getConstructResponseById(id), HttpStatus.OK);
     }
 
@@ -34,6 +35,21 @@ public class SupplyController {
     @PutMapping("/construct/{id}")
     public ResponseEntity<?> update(@PathVariable Long id, @RequestBody ConstructRequest constructRequest) {
         return new ResponseEntity<>(supplyService.update(id, constructRequest), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/memory/{id}")
+    public ResponseEntity<?> getMemoryResponseById(@PathVariable Long id) {
+        return new ResponseEntity<>(supplyService.getMemoryResponseById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/memory")
+    public ResponseEntity<?> create(@RequestBody MemoryRequest memoryRequest) {
+        return new ResponseEntity<>(supplyService.create(memoryRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/memory/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody MemoryRequest memoryRequest) {
+        return new ResponseEntity<>(supplyService.update(id, memoryRequest), HttpStatus.ACCEPTED);
     }
 
 }
