521a9820e8799b790f26f4bb3ec08bea263393e3
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 21:27:20 +0300

    Update UserResponse.java

--- src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/dto/user/UserResponse.java
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/dto/user/UserResponse.java
@@ -1,6 +1,7 @@
 package com.omaryusufonalan.pgrrndsimulatorbackend.dto.user;
 
 import com.omaryusufonalan.pgrrndsimulatorbackend.dto.currency.CurrencyWithoutUserResponse;
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.inventory.InventoryWithoutUserResponse;
 import com.omaryusufonalan.pgrrndsimulatorbackend.enums.Role;
 import lombok.*;
 
@@ -13,7 +14,12 @@ import java.util.Set;
 @ToString
 public class UserResponse {
     private Long id;
+
     private String username;
+
     private Role role;
+
     private Set<CurrencyWithoutUserResponse> currencies;
+
+    private InventoryWithoutUserResponse inventory;
 }
