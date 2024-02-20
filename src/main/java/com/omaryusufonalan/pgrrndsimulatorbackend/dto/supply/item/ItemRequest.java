b6904dd5e3dc5d7d5ddca22e78f761cd3088f186
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 14:06:13 +0300

    Create ItemRequest.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/dto/supply/item/ItemRequest.java
@@ -0,0 +1,11 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.item;
+
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.SupplyRequest;
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ItemRequest extends SupplyRequest {
+}
