8e4f9b5cf169ee6e25aa1e86cef30632cf5ca473
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 17:15:37 +0300

    Create DropDetailResponse.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/dto/dropdetail/DropDetailResponse.java
@@ -0,0 +1,30 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.dto.dropdetail;
+
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.banner.BannerWithoutAnythingResponse;
+import com.omaryusufonalan.pgrrndsimulatorbackend.enums.ItemType;
+import com.omaryusufonalan.pgrrndsimulatorbackend.enums.SupplyRarity;
+import com.omaryusufonalan.pgrrndsimulatorbackend.enums.SupplyType;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class DropDetailResponse {
+    private Long id;
+
+    private SupplyType supplyType;
+
+    private SupplyRarity supplyRarity;
+
+    private ItemType itemType;
+
+    private double baseDropRate;
+
+    private int pityGuarantee;
+
+    private int shardsObtainedFromDuplicate;
+
+    private BannerWithoutAnythingResponse banner;
+}
