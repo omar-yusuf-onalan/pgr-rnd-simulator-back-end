4e71813df557a78382d821913755ce67cd82a9c1
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 21:27:22 +0300

    Create InventoryRequest.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/dto/inventory/InventoryRequest.java
@@ -0,0 +1,12 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.dto.inventory;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class InventoryRequest {
+    private UserInInventoryRequest user;
+}
