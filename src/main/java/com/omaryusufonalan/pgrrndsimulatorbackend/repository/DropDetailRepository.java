4222ba82e289a3dffe3fb4c6a4141cdc081857d2
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 17:20:32 +0300

    Update DropDetailRepository.java

--- src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/repository/DropDetailRepository.java
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/repository/DropDetailRepository.java
@@ -1,4 +1,9 @@
 package com.omaryusufonalan.pgrrndsimulatorbackend.repository;
 
-public interface DropDetailRepository {
+import com.omaryusufonalan.pgrrndsimulatorbackend.entity.DropDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DropDetailRepository extends JpaRepository<DropDetail, Long> {
 }
