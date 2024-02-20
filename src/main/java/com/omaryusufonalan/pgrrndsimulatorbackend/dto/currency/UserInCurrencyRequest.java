cb3e31559dd027d25e2f9ff714585e0ed3fd9172
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 10:08:30 +0300

    Create UserInCurrencyRequest.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/dto/currency/UserInCurrencyRequest.java
@@ -0,0 +1,21 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.dto.currency;
+
+import com.omaryusufonalan.pgrrndsimulatorbackend.enums.Role;;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class UserInCurrencyRequest {
+    @PositiveOrZero
+    private Long id;
+
+    @NotBlank(message = "Username cannot be blank")
+    private String username;
+
+    private Role role;
+}
