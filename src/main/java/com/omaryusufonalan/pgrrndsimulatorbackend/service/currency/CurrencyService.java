f643ce8330e404ea0c4c98edbe14ff4181b3f70b
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 10:10:22 +0300

    Create CurrencyService.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/service/currency/CurrencyService.java
@@ -0,0 +1,9 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.service.currency;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CurrencyService {
+}
