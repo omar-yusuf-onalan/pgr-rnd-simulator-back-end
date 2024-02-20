3889981f1b542b0f8beb697a64f6d8b91fc1b92e
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 10:07:34 +0300

    Add initializePlayerCurrencies method

--- src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/entity/user/Player.java
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/entity/user/Player.java
@@ -1,13 +1,41 @@
 package com.omaryusufonalan.pgrrndsimulatorbackend.entity.user;
 
 import com.omaryusufonalan.pgrrndsimulatorbackend.entity.Currency;
+import com.omaryusufonalan.pgrrndsimulatorbackend.enums.CurrencyType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @DiscriminatorValue("PLAYER")
-public class Player {
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Player extends User {
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "player", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private Set<Currency> currencies;
+
+    public Player initializePlayerCurrencies() {
+        CurrencyType[] currencyTypes = CurrencyType.values();
+        Set<Currency> playerCurrencies = new HashSet<>();
+
+        for (CurrencyType currencyType : currencyTypes) {
+            playerCurrencies.add(new Currency(
+                    null,
+                    0,
+                    currencyType,
+                    this
+            ));
+        }
+
+        this.setCurrencies(playerCurrencies);
+
+        return this;
+    }
 }
