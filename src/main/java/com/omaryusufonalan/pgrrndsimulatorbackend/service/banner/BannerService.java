63557a0203c391f8498e56a878e9c6ab6c952bb7
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 16:18:34 +0300

    Create BannerService.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/service/banner/BannerService.java
@@ -0,0 +1,9 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.service.banner;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BannerService {
+}
