bd9010660710ca415fdaf0c56710fd510b14c427
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 16:13:46 +0300

    Create BannerType.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/enums/BannerType.java
@@ -0,0 +1,11 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.enums;
+
+public enum BannerType {
+    ARRIVAL_CONSTRUCT,
+    FATE_ARRIVAL_CONSTRUCT,
+    TARGET_MEMBER,
+    TARGET_WEAPON,
+    BASIC_WEAPON,
+    TARGET_UNIFRAME,
+    TARGET_CUB
+}
