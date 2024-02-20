893deec2317e6a7f23f0b3695671b9bb566f563d
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 14:06:14 +0300

    Create ItemResponse.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/dto/supply/item/ItemResponse.java
@@ -0,0 +1,14 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.item;
+
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.SupplyResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ItemResponse extends SupplyResponse {
+}
