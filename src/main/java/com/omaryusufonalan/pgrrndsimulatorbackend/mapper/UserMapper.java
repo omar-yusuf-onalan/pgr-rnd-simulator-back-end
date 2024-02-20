fa6e5876364c1448747ee247661085793919fd39
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 10:07:47 +0300

    Add asPlayer method

--- src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/mapper/UserMapper.java
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.omaryusufonalan.pgrrndsimulatorbackend.mapper;
 
 import com.omaryusufonalan.pgrrndsimulatorbackend.dto.user.UserRequest;
 import com.omaryusufonalan.pgrrndsimulatorbackend.dto.user.UserResponse;
+import com.omaryusufonalan.pgrrndsimulatorbackend.entity.user.Player;
 import com.omaryusufonalan.pgrrndsimulatorbackend.entity.user.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
@@ -9,6 +10,8 @@ import org.mapstruct.MappingTarget;
 @Mapper
 public interface UserMapper {
     User asUser(UserRequest userRequest);
+
+    Player asPlayer(UserRequest userRequest);
 
     UserResponse asUserResponse(User user);
 
