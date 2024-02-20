ce1202ed4458a8421eaf7a377d19fb4eaa2fa616
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 16:16:32 +0300

    Create BannerRequest.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/dto/banner/BannerRequest.java
@@ -0,0 +1,22 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.dto.banner;
+
+import com.omaryusufonalan.pgrrndsimulatorbackend.enums.BannerType;
+import com.omaryusufonalan.pgrrndsimulatorbackend.enums.CurrencyType;
+import com.omaryusufonalan.pgrrndsimulatorbackend.enums.ResearchType;
+import com.omaryusufonalan.pgrrndsimulatorbackend.enums.SupplyType;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class BannerRequest {
+    private ResearchType researchType;
+
+    private BannerType bannerType;
+
+    private CurrencyType currencyType;
+
+    private SupplyType primarySupply;
+}
