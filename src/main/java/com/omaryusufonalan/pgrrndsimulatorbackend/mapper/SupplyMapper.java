e9536e89f9eaaece7ea3e24746c75a793818a49a
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 14:06:16 +0300

    Create SupplyMapper.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/mapper/SupplyMapper.java
@@ -0,0 +1,28 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.mapper;
+
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.construct.ConstructRequest;
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.construct.ConstructResponse;
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.item.ItemRequest;
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.item.ItemResponse;
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.memory.MemoryRequest;
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.memory.MemoryResponse;
+import com.omaryusufonalan.pgrrndsimulatorbackend.entity.supply.Construct;
+import com.omaryusufonalan.pgrrndsimulatorbackend.entity.supply.Item;
+import com.omaryusufonalan.pgrrndsimulatorbackend.entity.supply.Memory;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper
+public interface SupplyMapper {
+    Construct asConstruct(ConstructRequest constructRequest);
+    ConstructResponse asConstructResponse(Construct construct);
+    void updateConstruct(@MappingTarget Construct construct, ConstructRequest constructRequest);
+
+    Memory asMemory(MemoryRequest memoryRequest);
+    MemoryResponse asMemoryResponse(Memory memory);
+    void updateMemory(@MappingTarget Memory memory, MemoryRequest memoryRequest);
+
+    Item asItem(ItemRequest itemRequest);
+    ItemResponse asItemResponse(Item item);
+    void updateItem(@MappingTarget Item item, ItemRequest itemRequest);
+}
