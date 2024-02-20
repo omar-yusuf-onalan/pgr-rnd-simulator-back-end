58ef42a85cc614c78828cdb7dd2da26d18e487eb
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 14:06:07 +0300

    Create MemoryRequest.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/dto/supply/memory/MemoryRequest.java
@@ -0,0 +1,14 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.memory;
+
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.supply.SupplyRequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MemoryRequest extends SupplyRequest {
+}
