f01578ab4b293715b66bc2a0756d2089eff41cda
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 16:21:31 +0300

    Implement BannerCRUD

--- src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/service/banner/BannerService.java
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/service/banner/BannerService.java
@@ -1,9 +1,44 @@
 package com.omaryusufonalan.pgrrndsimulatorbackend.service.banner;
 
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.banner.BannerRequest;
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.banner.BannerResponse;
+import com.omaryusufonalan.pgrrndsimulatorbackend.entity.Banner;
+import com.omaryusufonalan.pgrrndsimulatorbackend.mapper.BannerMapper;
+import com.omaryusufonalan.pgrrndsimulatorbackend.repository.BannerRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class BannerService {
+public class BannerService implements BannerCRUD {
+    private final BannerRepository bannerRepository;
+    private final BannerMapper bannerMapper;
+
+    @Override
+    public Banner getById(Long id) {
+        return bannerRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Banner with ID " + id + " not found"));
+    }
+
+    @Override
+    public BannerResponse create(BannerRequest bannerRequest) {
+        Banner bannerToBeCreated = bannerMapper.asBanner(bannerRequest);
+
+        return bannerMapper.asBannerResponse(bannerRepository.save(bannerToBeCreated));
+    }
+
+    @Override
+    public BannerResponse update(Long id, BannerRequest bannerRequest) {
+        Banner bannerToBeUpdated = getById(id);
+
+        bannerMapper.update(bannerToBeUpdated, bannerRequest);
+
+        return bannerMapper.asBannerResponse(bannerRepository.save(bannerToBeUpdated));
+    }
+
+    @Override
+    public void delete(Long id) {
+        bannerRepository.delete(getById(id));
+    }
 }
