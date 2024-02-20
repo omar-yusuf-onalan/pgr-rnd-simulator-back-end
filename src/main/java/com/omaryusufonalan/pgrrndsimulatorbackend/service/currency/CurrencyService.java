0eb4982b661c1f0d610f73db71a660be392a2885
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 10:15:25 +0300

    Implement CurrencyCRUD

--- src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/service/currency/CurrencyService.java
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/service/currency/CurrencyService.java
@@ -1,9 +1,44 @@
 package com.omaryusufonalan.pgrrndsimulatorbackend.service.currency;
 
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.currency.CurrencyRequest;
+import com.omaryusufonalan.pgrrndsimulatorbackend.dto.currency.CurrencyResponse;
+import com.omaryusufonalan.pgrrndsimulatorbackend.entity.Currency;
+import com.omaryusufonalan.pgrrndsimulatorbackend.mapper.CurrencyMapper;
+import com.omaryusufonalan.pgrrndsimulatorbackend.repository.CurrencyRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CurrencyService {
+public class CurrencyService implements CurrencyCRUD {
+    private final CurrencyRepository currencyRepository;
+    private final CurrencyMapper currencyMapper;
+
+    @Override
+    public Currency getById(Long id) {
+        return currencyRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Currency with ID " + id + " not found"));
+    }
+
+    @Override
+    public CurrencyResponse create(CurrencyRequest currencyRequest) {
+        Currency currencyToBeCreated = currencyMapper.asCurrency(currencyRequest);
+
+        return currencyMapper.asCurrencyResponse(currencyRepository.save(currencyToBeCreated));
+    }
+
+    @Override
+    public CurrencyResponse update(Long id, CurrencyRequest currencyRequest) {
+        Currency currencyToBeUpdated = getById(id);
+
+        currencyMapper.update(currencyToBeUpdated, currencyRequest);
+
+        return currencyMapper.asCurrencyResponse(currencyRepository.save(currencyToBeUpdated));
+    }
+
+    @Override
+    public void delete(Long id) {
+        currencyRepository.delete(getById(id));
+    }
 }
