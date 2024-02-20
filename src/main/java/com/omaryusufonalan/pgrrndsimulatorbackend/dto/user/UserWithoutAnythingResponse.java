6faac2031fabe02495a83461a3d2400f1cf68b59
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 10:08:13 +0300

    Create UserWithoutAnythingResponse.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/dto/user/UserWithoutAnythingResponse.java
@@ -0,0 +1,15 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.dto.user;
+
+import com.omaryusufonalan.pgrrndsimulatorbackend.enums.Role;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class UserWithoutAnythingResponse {
+    private Long id;
+    private String username;
+    private Role role;
+}
