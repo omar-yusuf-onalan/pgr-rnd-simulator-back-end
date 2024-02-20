c7ec6b36457d638c0bca833148dcbfa1a83b1291
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 14:07:17 +0300

    Create SupplyService.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/service/supply/SupplyService.java
@@ -0,0 +1,9 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.service.supply;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SupplyService {
+}
