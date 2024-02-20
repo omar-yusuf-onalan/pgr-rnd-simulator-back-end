9ee90653b12dd9f3fcebcfbf19fb3eff46384bd8
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-20 13:52:39 +0300

    Create Memory.java

---
+++ src/main/java/com/omaryusufonalan/pgrrndsimulatorbackend/entity/supply/Memory.java
@@ -0,0 +1,13 @@
+package com.omaryusufonalan.pgrrndsimulatorbackend.entity.supply;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+public class Memory extends Supply {
+}
